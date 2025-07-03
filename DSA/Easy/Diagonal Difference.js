0a9de9e2dfecd672939834cfbc27e3fdd4f877e2
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-07-03 22:19:09 +0530

    Update Diagonal Difference.js

--- DSA/Easy/Diagonal Difference.js
+++ DSA/Easy/Diagonal Difference.js
@@ -1,9 +1,1 @@
-function diagonalDifference(arr) {
-    let n = arr.length;
-    let primarySum = 0;
-    let secondarySum = 0;
-
-    for (let i = 0; i < n; i++) {
-        primarySum += arr[i][i];
-        secondarySum += arr[i][n - 1 - i];
 
