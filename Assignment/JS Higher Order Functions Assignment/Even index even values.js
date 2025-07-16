26ab846c750ded0ef8f7926bf5a0930bd6465faa
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-07-16 21:21:36 +0530

    Update Even index even values.js

--- Assignment/JS Higher Order Functions Assignment/Even index even values.js
+++ Assignment/JS Higher Order Functions Assignment/Even index even values.js
@@ -1,4 +1,12 @@
 function evenIndexAndEvenValue(arr) {
+  return arr.filter((num, index) => index % 2 === 0 && num % 2 === 0);
+}
+
+
+const input = [2, 4, 5, 3, 6, 8];
+const output = evenIndexAndEvenValue(input);
+
+console.log(output);
 
 
 
